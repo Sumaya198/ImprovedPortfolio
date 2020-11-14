4f6e4b0f59d1938882797b85fa3bc91b3cc37118
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-11-14 08:42:53 +0000

    updates html css and js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  
     $(window).on('scroll',function(){
         var scroll = $(window).scrollTop();
         if(scroll>=50){
@@ -7,4 +9,55 @@ $(document).ready(function(){
          $('.sticky').removeClass("stickyadd");
         }
     })
-})
+
+  var typed = new Typed(".element",{
+      strings: ["Sumaya Hassan", "A Web Developer", "A Graphic Designer"],
+      smartBackspace: true,
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+      loopCount: Infinity,
+      startDelay: 1000,
+  });
+
+  var $child = $('.way-fade-up').children();
+$child.each(function(){
+  var self= $(this);
+  $(this).waypoint(function(){
+    self.addClass('animated fadeInUp');
+  },{offset: '80%'});
+});
+
+var $child = $('.way-fade-left').children();
+$child.each(function(){
+  var self= $(this);
+  $(this).waypoint(function(){
+    self.addClass('animated fadeInLeft');
+  },{offset: '90%'});
+});
+
+var $child = $('.way-fade-right').children();
+$child.each(function(){
+  var self= $(this);
+  $(this).waypoint(function(){
+    self.addClass('animated fadeInRight');
+  },{offset: '90%'});
+});
+
+
+  // owl carousel
+
+  
+    $(".owl-carousel").owlCarousel({
+       loop:true,
+       autoplay:true,
+       autoplayTimeout: 4000,
+       items: 1
+    });
+ 
+    $('a').smoothScroll({
+
+      speed:2000,
+    });
+
+});
